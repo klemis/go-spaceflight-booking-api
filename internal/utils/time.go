08495ee6e4b2b7673ace2874125d38d1bfae662a
Kamil Lemisz <kamil.lemisz@gmail.com>
2024-09-02 18:24:16 +0200

    feat: create date range based on provided date

---
+++ internal/utils/time.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+)
+
+// GetRangeQueryValues creates a range query for a given date to cover the entire day.
+func GetRangeQueryValues(dateTime time.Time) (gte, lt string) {
+	// Truncate to the start of the day (midnight)
+	startOfDay := dateTime.Truncate(24 * time.Hour)
+
+	// Add 1 day to get the start of the next day
+	endOfDay := startOfDay.Add(24 * time.Hour)
+
+	gte = fmt.Sprintf(`%s`, startOfDay.Format(time.RFC3339))
+	lt = fmt.Sprintf(`%s`, endOfDay.Format(time.RFC3339))
+
+	return
+}
