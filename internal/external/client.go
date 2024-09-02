4d27494e542262fc0538e033f94d7bf391ad71a4
Kamil Lemisz <kamil.lemisz@gmail.com>
2024-09-02 18:14:44 +0200

    feat: external spacex client with CheckLaunchpadAvailability endpoint

---
+++ internal/external/client.go
@@ -0,0 +1,50 @@
+package external
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/klemis/go-spaceflight-booking-api/models"
+	"net/http"
+)
+
+// SpaceXAPIClient represents a client for interacting with the SpaceX API.
+type SpaceXAPIClient struct {
+	Client  *http.Client
+	BaseURL string
+}
+
+// NewSpaceXAPIClient creates a new instance of SpaceXAPIClient with the specified base URL.
+func NewSpaceXAPIClient(baseURL string) *SpaceXAPIClient {
+	return &SpaceXAPIClient{
+		Client:  http.DefaultClient,
+		BaseURL: baseURL,
+	}
+}
+
+// CheckLaunchpadAvailability checks if there are any launches available for the given request body.
+func (c *SpaceXAPIClient) CheckLaunchpadAvailability(body models.RequestBody) (bool, error) {
+	url := c.BaseURL + "launches/query"
+
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return false, fmt.Errorf("failed to marshal request body: %w", err)
+	}
+
+	resp, err := c.Client.Post(url, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return false, fmt.Errorf("failed to send request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	var result models.Launches
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return false, fmt.Errorf("failed to decode response body: %w", err)
+	}
+
+	return len(result.Docs) > 0, nil
+}
