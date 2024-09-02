0ed7e7f4e5137d3c8b46e357aff680f0e96ca9d1
Kamil Lemisz <kamil.lemisz@gmail.com>
2024-09-02 18:25:07 +0200

    feat: create handler for booking service

---
+++ internal/api/handler.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/klemis/go-spaceflight-booking-api/internal/service"
+	"github.com/klemis/go-spaceflight-booking-api/models"
+)
+
+type Handler struct {
+	BookingService service.BookingService
+}
+
+// NewHandler creates a new Handler with the provided BookingService.
+func NewHandler(bookingService service.BookingService) *Handler {
+	return &Handler{
+		BookingService: bookingService,
+	}
+}
+
+// CreateBooking handles the creation of a new booking.
+func (h *Handler) CreateBooking(c *gin.Context) {
+	var booking models.BookingRequest
+	if err := c.BindJSON(&booking); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	result, err := h.BookingService.CreateBooking(booking)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not create booking " + err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
--- main.go
+++ main.go
@@ -6,17 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/klemis/go-spaceflight-booking-api/internal/api"
+	"github.com/klemis/go-spaceflight-booking-api/internal/external"
+	"github.com/klemis/go-spaceflight-booking-api/internal/service"
 )
 
 func main() {
 	log.Println("Starting API server...")
+	// Initialize spacex client.
+	externalClient := external.NewSpaceXAPIClient("https://api.spacexdata.com/v4/")
+	// Initialize the booking service with the spacex external client.
+	bookingService := service.NewBookingService(externalClient)
+	// Initialize the handler with the booking service.
+	handler := api.NewHandler(bookingService)
 
-	r := gin.New()
-
-	api.SetupRouter(r)
+	router := gin.Default()
+	v1 := router.Group("/api/v1")
+	v1.POST("/bookings", handler.CreateBooking)
 
 	log.Println("API server listening on port 8080...")
-	err := r.Run(":8080")
+	err := router.Run(":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
