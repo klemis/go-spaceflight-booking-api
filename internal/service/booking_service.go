a360ce41dc7076141d209fef939c165744158bf3
Kamil Lemisz <kamil.lemisz@gmail.com>
2024-09-02 18:20:19 +0200

    feat: draft CreateBooking method

--- internal/service/booking_service.go
+++ internal/service/booking_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/klemis/go-spaceflight-booking-api/internal/external"
+	"github.com/klemis/go-spaceflight-booking-api/internal/utils"
 	"github.com/klemis/go-spaceflight-booking-api/models"
 )
 
@@ -20,4 +21,34 @@ func NewBookingService(externalClient *external.SpaceXAPIClient) BookingService 
 	return &bookingService{
 		externalClient: externalClient,
 	}
+}
+
+// CreateBooking creates a new booking.
+func (s *bookingService) CreateBooking(request models.BookingRequest) (models.BookingResponse, error) {
+	gte, lt := utils.GetRangeQueryValues(request.LaunchDate)
+
+	body := models.RequestBody{
+		Query: map[string]interface{}{
+			"launchpad": request.LaunchpadID,
+			"date_utc": map[string]interface{}{
+				"$gte": gte,
+				"$lt":  lt,
+			},
+		},
+		Options: models.Options{
+			Select: "id",
+		},
+	}
+	available, err := s.externalClient.CheckLaunchpadAvailability(body)
+	if err != nil {
+		return models.BookingResponse{}, err
+	}
+	if !available {
+		return models.BookingResponse{}, err
+	}
+
+	return models.BookingResponse{
+		ID:          123,
+		LaunchpadID: request.LaunchpadID,
+	}, nil
 }
